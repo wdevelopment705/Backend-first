3fa6a709ebe92ebf8d36ce75984dfa5aea3d6338
wdevelopment705 <wdevelopment705@gmail.com>
2025-12-27 14:40:36 +0500

    m

--- backend/db.js
+++ backend/db.js
@@ -5,7 +5,13 @@ export const db = mysql.createPool({
   user: process.env.DB_USER,       
   password: process.env.DB_PASSWORD, 
   database: process.env.DB_NAME,   
-  port: process.env.DB_PORT,       
+  port: Number(process.env.DB_PORT),  
+
+  ssl: {
+    rejectUnauthorized: false
+  },
+
+  connectTimeout: 10000, // 10 sec
   waitForConnections: true,
   connectionLimit: 10,
 });
